0f8ce6e7449a173a0036522d13b97faa60e9741d
Vadim-Maletskii <118855200+Vadim-Maletskii@users.noreply.github.com>
2022-12-04 18:41:11 +0100

    Update 01-RCurl.R
    reveals identity

--- scripts/01-RCurl.R
+++ scripts/01-RCurl.R
@@ -3,7 +3,10 @@ library(tidyverse)
 
 # Loading the page ----
 rcurl_url <- 'https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/'
-rcurl_page <- RCurl::getURL(url = rcurl_url)
+email <- "wiscain@yandex.ru"
+agent <- R.Version()$version.string
+user_id <- c(From = email, `User-Agent` = agent)
+rcurl_page <- RCurl::getURL(url = rcurl_url,  httpheader= (user_id))
 
 # Getting the links ----
 links <- XML::getHTMLLinks(rcurl_page)
