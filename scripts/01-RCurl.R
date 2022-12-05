d98fa45ba4ecc1ee35aa9d85799a720c75a508e9
Vadim Maletskii <wiscain@yandex.ru>
2022-12-05 17:48:01 +0100

    Comments the code

--- scripts/01-RCurl.R
+++ scripts/01-RCurl.R
@@ -11,3 +11,8 @@ rcurl_page <- RCurl::getURL(url = rcurl_url,  httpheader= (user_id))
 # Getting the links ----
 links <- XML::getHTMLLinks(rcurl_page)
 links_filtered <- stringr::str_subset(links, pattern = '^https://beppegrillo\\.it[[:graph:]]') %>% unique()
+
+# it is pointless to further filtering with the help of regexps
+# because the list consists of only 28 links and it is to hard to create a search pattern
+# as there are some links with just names and again should be subseted manualy even with regexps
+# for example : "https://beppegrillo.it/cookie-policy/" or "https://beppegrillo.it/grilloteca/" (which actually is a part of "categories" of the website)
