e7aa15b8c6a9dc006e14cd08ab41b94cee9987dd
skorotkikh <s.korotkikh21@gmail.com>
2022-12-06 12:53:09 +0100

    Downloads the pages and scrapes the main text

--- scripts/02-archive_pages.R
+++ scripts/02-archive_pages.R
@@ -11,19 +11,21 @@ for (i in 1:47) {
   archive_links[[i]] <- html_attr(html_nodes(page, '.td_module_10 a'), 'href') %>% unique()
 }
 
-# Downloading the pages
-
+# Downloading the pages ----
+counter = 1  # it is used to save the page with its own number
 for (i in 1:47) {
   for (j in 1:length(archive_links[[i]])) {
     grillo_page <- RCurl::getURL(url = archive_links[[i]][j],  httpheader= (user_id))
-    download.file(url = archive_links[[1]][1], destfile = here::here('pages', str_c('page_', 1, '.html')))
+    download.file(url = archive_links[[i]][j], destfile = here::here('pages', str_c('page_', counter, '.html')))
+    counter = counter + 1
+    Sys.sleep(0.5)
   }
 }
-)
 
-# session <- rvest::session(link,
-#                           add_headers(
-#                             From = email, 
-#                             `User-Agent` = user_agent
-#                           )
-# )
+# Extracting the main text from the pages ----
+main_texts <- vector(mode = 'list', length = 470)
+for (i in 1:470) {
+  main_texts[[i]] <- rvest::read_html(here::here('pages', str_c('page_', i, '.html'))) %>% 
+    html_elements(css = "p") %>% 
+    html_text()
+}
