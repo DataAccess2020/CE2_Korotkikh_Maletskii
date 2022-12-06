f0421b456ea3e636b2afa92a71f5ff03fde68185
skorotkikh <s.korotkikh21@gmail.com>
2022-12-06 11:36:02 +0100

    Elaborates the code (task 4b)

--- scripts/02-archive_pages.R
+++ scripts/02-archive_pages.R
@@ -11,6 +11,16 @@ for (i in 1:47) {
   archive_links[[i]] <- html_attr(html_nodes(page, '.td_module_10 a'), 'href') %>% unique()
 }
 
+# Downloading the pages
+
+for (i in 1:47) {
+  for (j in 1:length(archive_links[[i]])) {
+    grillo_page <- RCurl::getURL(url = archive_links[[i]][j],  httpheader= (user_id))
+    download.file(url = archive_links[[1]][1], destfile = here::here('pages', str_c('page_', 1, '.html')))
+  }
+}
+)
+
 # session <- rvest::session(link,
 #                           add_headers(
 #                             From = email, 
