7ea94724d8a9cd77e920376f9bd3800012f27484
Vadim Maletskii <wiscain@yandex.ru>
2022-12-06 14:14:16 +0100

    final filtering

--- scripts/01-RCurl.R
+++ scripts/01-RCurl.R
@@ -12,7 +12,10 @@ rcurl_page <- RCurl::getURL(url = rcurl_url,  httpheader= (user_id))
 links <- XML::getHTMLLinks(rcurl_page)
 links_filtered <- stringr::str_subset(links, pattern = '^https://beppegrillo\\.it[[:graph:]]') %>% unique()
 
-# it is pointless to further filtering with the help of regexps
+# It is pointless to further filtering with the help of regexps
 # because the list consists of only 28 links and it is to hard to create a search pattern
 # as there are some links with just names and again should be subseted manualy even with regexps
 # for example : "https://beppegrillo.it/cookie-policy/" or "https://beppegrillo.it/grilloteca/" (which actually is a part of "categories" of the website)
+
+links_filtered_2 = as_data_frame(links_filtered)
+links_filtered_final = links_filtered_2[c(18,19,21,22,23,24,25,26,27), ]
